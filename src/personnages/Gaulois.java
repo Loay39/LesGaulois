a20e7bce77395f5110de44d5f23a7c463a7842dd
Loay Younes <loay.younes@univ-tlse3.fr>
2023-09-21 15:08:25 +0200

    test commit

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,27 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;		
+	}
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	public String getNom() {
+		return nom;
+	}
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+	
+	public void name() {
+	p	
+	}
+}
+
